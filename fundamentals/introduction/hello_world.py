99fd0bcebf0e571d53bf9f2cf19b8d0944cd7ebb
Judah Kahler <judah.kahler@gmail.com>
2023-10-23 10:35:02 -0500

    intialization

---
+++ fundamentals/introduction/hello_world.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+x = "Hello Python"
+print(x)
+y = 42
+print(y)
+
+# indentation practice
+
+x = 10
+if x > 50:
+    print("bigger than 50.")
+else:
+    print("smaller than 50")
+
+# pass practice
+
+class EmptyClass:
+    pass
+
+for val in my_string:
+    pass
+
+
