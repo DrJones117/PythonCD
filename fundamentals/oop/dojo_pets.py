541612e535253b07a44eb89e160beede91d5c20d
Judah Kahler <judah.kahler@gmail.com>
2023-10-25 10:42:59 -0500

    algos

---
+++ fundamentals/oop/dojo_pets.py
@@ -0,0 +1,30 @@
+class Ninja:
+    def __init__(self, first_name, last_name, treats, pet_food, pet):
+        self.first = first_name
+        self.last = last_name
+        self.treats = treats
+        self.pet_food = pet_food
+        self.pet = pet
+
+    def walk(self):
+        print("I'll take him out.")
+
+    def feed(self):
+        
+
+class Pet:
+    def __init__(name, type, tricks):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+
+    def sleep(self):
+
+    def eat(self):
+
+    def play(self):
+
+    def noise(self):
+
+
+ninja1 = Ninja()
