a82e7bfc1566b90c2d7341907c296d1296aefd15
Judah Kahler <judah.kahler@gmail.com>
2023-10-26 18:55:11 -0500

    Cecker Board finished

--- flask/fundamentals/CheckerBoard/server.py
+++ flask/fundamentals/CheckerBoard/server.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/<int:num>/<int:num2>')
+def standard(num, num2):
+    return render_template('index.html', num=num, num2=num2)
 
 if __name__=="__main__":
     app.run(debug=True)
