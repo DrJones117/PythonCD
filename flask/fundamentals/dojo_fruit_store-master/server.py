2aa618d0abf993ef7e5b9d1a8b2750cb32b1d82b
Judah Kahler <judah.kahler@gmail.com>
2023-10-27 12:29:38 -0500

    Dojo fruit store finished

--- flask/fundamentals/dojo_fruit_store-master/server.py
+++ flask/fundamentals/dojo_fruit_store-master/server.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
-@app.route('/')         
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST'])
 def checkout():
     print(request.form)
-    return render_template("checkout.html",)
+    print(int(request.form["strawberry"]) + int(request.form["raspberry"]) + int(request.form["apple"]))
+    return render_template("checkout.html", first=request.form["first_name"], last=request.form["last_name"], 
+    id=request.form["student_id"], one=request.form["strawberry"], two=request.form["raspberry"], three=request.form["apple"])
 
-@app.route('/fruits')         
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
