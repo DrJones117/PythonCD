84a70f6d615ebf91d6ae9c552fe9d3fc99aa037b
Judah Kahler <judah.kahler@gmail.com>
2023-11-09 14:58:30 -0600

    Getting github finished

--- ajax/ajax_api/getting_github/script.js
+++ ajax/ajax_api/getting_github/script.js
@@ -1,8 +1,14 @@
 async function getCoderData() {
         var response = await fetch("https://api.github.com/users/DrJones117");
         var coderData = await response.json();
+        console.log(coderData);
+        console.log(Array.isArray(coderData));
+        console.log(Array.isArray(coderData.email));
+
+        var userName = document.getElementById("name");
+        var profile = document.getElementById("profile");
+        userName.innerHTML = `${coderData.login} has ${coderData.followers} followers`;
+        profile.innerHTML = `<img src="${coderData.avatar_url}" alt="profile_image">`;
         return coderData;
     }
-        
-    console.log(getCoderData());
-    
+
